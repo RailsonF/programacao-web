001079851d40bd927d566ccc554380d9eee3899b
Railson Furtado <railsonfurtado16@gmail.com>
2024-11-14 21:41:59 -0300

    changes of class

---
+++ ativitys/att3/js/script.js
@@ -0,0 +1,10 @@
+function mudarCorDoBody() {
+    let escolha = prompt("Informe a cor de fundo da página")
+    let body = document.body
+    body.style.background = "escolha"
+
+}
+
+//mudarCorDoBody()
+let escolha = prompt("Informe a cor de fundo da página")
+console.log("escolha")
